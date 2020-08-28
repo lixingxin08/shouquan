051e351b413b103946ff6c34cf5952cf46a78463
lixingxin08 <lixingxin08@183.com>
2020-08-28 11:06:49 +0800

    8.28

---
+++ src/components/tree/tree.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="search">
+    <div class="search">
+      <a-input-search placeholder enter-button="搜索" size="default" @search="onSearch" />
+    </div>
+    <div class="istree">
+      <a-tree :show-line="showLine" @select="onSelect" :tree-data="treedata" :replaceFields='replaceFields' :default-expanded-keys="defaultExpandedKeys">
+        <a-icon slot="icon" type="carry-out" />
+      </a-tree>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showLine: true,
+      showIcon: true, 
+    };
+  },
+  props: {
+    treedata: Array,//树数据
+    replaceFields:Object,//替换属性
+    defaultExpandedKeys:Array//默认展开
+  },
+  created() {
+  },
+  methods: {
+    onSelect(selectedKeys,selectedNodes) {
+       this.$emit("selectdata", selectedNodes.selectedNodes[0].data.props||'');
+    },
+    onSearch(value) {
+      this.$emit("searchdata", value);
+    },
+  },
+};
+</script>
+<style scoped>
+#search {
+  width: 280px;
+  height: 840px;
+  padding: 20px;
+  background-color: #fff;
+}
+.search {
+  width: 240px;
+}
+.istree {
+  text-align: left;
+  height: 100%;
+  overflow-x: hidden;
+  overflow-y: scroll;
+}
+.istree::-webkit-scrollbar {
+  display: none;
+}
+</style>
--- src/views/base/administrativedivision/administrativedivision.vue
+++ src/views/base/administrativedivision/administrativedivision.vue
@@ -1,22 +1,150 @@
 <template>
-  <div>administrativedivision
-
-      <div class="tree" @click="gettree()">gettree</div>
+  <div class="administrativedivision flex_fs">
+    <is-left
+      :treedata="treedata"
+      :replaceFields="replaceFields"
+      :defaultExpandedKeys="defaultExpandedKeys"
+      @selectdata="getselectdata"
+    ></is-left>
+    <div class="tree" @click="gettree()">gettree</div>
   </div>
 </template>
 <script>
+import isLeft from "../../../components/tree/tree.vue";
 export default {
+  components: {
+    isLeft,
+  },
+  data() {
+    return {
+      treedata: null,
+      isselectdata: "",
+      replaceFields: {
+        title: "name",
+        key: "id",
+      },
+      defaultExpandedKeys: [],
+      data: [
+        {
+          id: "100000000000000000000000000000000000000000000000000000000000",
+          name: "中国",
+          isParent: true,
+          levelType: 1,
+          open: true,
+          pid: "0",
+        },
+        {
+          id: "100001000000000000000000000000000000000000000000000000000000",
+          name: "北京",
+          isParent: true,
+          levelType: 2,
+          open: true,
+          pid: "100000000000000000000000000000000000000000000000000000000000",
+        },
+        {
+          id: "100001001000000000000000000000000000000000000000000000000000",
+          name: "北京市",
+          isParent: true,
+          levelType: 3,
+          open: true,
+          pid: "100001000000000000000000000000000000000000000000000000000000",
+        },
+        {
+          id: "100001001001000000000000000000000000000000000000000000000000",
+          name: "东城区",
+          isParent: true,
+          levelType: 4,
+          open: true,
+          pid: "100001001000000000000000000000000000000000000000000000000000",
+        },
+        {
+          id: "100001001001001000000000000000000000000000000000000000000000",
+          name: "东四街道",
+          isParent: true,
+          levelType: 5,
+          open: false,
+          pid: "100001001001000000000000000000000000000000000000000000000000",
+        },
+        {
+          id: "100001001001001001000000000000000000000000000000000000000000",
+          name: "二条社区",
+          isParent: false,
+          levelType: 6,
+          open: false,
+          pid: "100001001001001000000000000000000000000000000000000000000000",
+        },
+        {
+          id: "100002000000000000000000000000000000000000000000000000000000",
+          name: "天津",
+          isParent: true,
+          levelType: 2,
+          open: true,
+          pid: "100000000000000000000000000000000000000000000000000000000000",
+        },
+        {
+          id: "100002001000000000000000000000000000000000000000000000000000",
+          name: "天津市",
+          isParent: false,
+          levelType: 3,
+          open: true,
+          pid: "100002000000000000000000000000000000000000000000000000000000",
+        },
+      ],
+    };
+  },
+  created() {
+    this.setdata();
+  },
   methods: {
-    gettree() {
-      this.$http.post(this.$api.areatree,{ areaId: "100000000000000000000000000000000000000000000000000000000000" }).then(res=>{
-          console.log(res,11);
+    toTree(data) {
+      let result = [];
+      if (!Array.isArray(data)) {
+        return result;
+      }
+      data.forEach((item) => {
+        delete item.children;
       });
+      let map = {};
+      data.forEach((item) => {
+        map[item.id] = item;
+      });
+      data.forEach((item) => {
+        let parent = map[item.pid];
+        if (parent) {
+          (parent.children || (parent.children = [])).push(item);
+        } else {
+          result.push(item);
+        }
+      });
+      return result;
+    },
+    setdata() {
+      for (let i = 0; i < this.data.length; i++) {
+        if (this.data[i].open == true) {
+          this.defaultExpandedKeys.push(this.data[i].id);
+        }
+      }
+      this.treedata = this.toTree(this.data);
+    },
+    getselectdata(val) {
+      this.isselectdata = val;
+      console.log(this.isselectdata, 3333);
+    },
+    gettree() {
+      this.$http
+        .post(this.$api.areatree, {
+          areaId:
+            "100000000000000000000000000000000000000000000000000000000000",
+        })
+        .then((res) => {
+          console.log(res, 11);
+        });
     },
   },
 };
 </script>
 <style  scoped>
-.tree{
-    text-align: left;
+.tree {
+  text-align: left;
 }
 </style>
