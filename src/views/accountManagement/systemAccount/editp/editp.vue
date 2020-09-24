d6b9dcbb3ab3deb40513c198d8834f15de162124
lixingxin08 <lixingxin08@163.com>
2020-09-24 11:24:34 +0800

    add

--- src/views/accountManagement/systemAccount/editp/editp.vue
+++ src/views/accountManagement/systemAccount/editp/editp.vue
@@ -36,7 +36,7 @@
           this.$message.warning('请输入修改的密码');
           return
         }
-        if (!this.password.length<6 || this.password2.length<6) {
+        if (this.password.length<6 || this.password2.length<6) {
           this.$message.warning('密码要求6-16位');
           return
         }
